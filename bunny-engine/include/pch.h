06f7c0b664ad5c7a03af1df04a67ba6f4a93f8a1
sunny <sunny.elliott@digipen.edu>
2026-02-19 02:08:24 -0800

    USD smoke test (that took a while...)

--- bunny-engine/include/pch.h
+++ bunny-engine/include/pch.h
@@ -1,6 +1,18 @@
 #pragma once
 
-#include <vulkan/vulkan.h>
+#include <pxr/pxr.h>
+
+#include <pxr/base/gf/matrix4d.h>
+#include <pxr/base/gf/vec3f.h>
+
+#include <pxr/usd/usd/stage.h>
+#include <pxr/usd/usdGeom/cone.h>
+#include <pxr/usd/usdGeom/cube.h>
+#include <pxr/usd/usdGeom/mesh.h>
+#include <pxr/usd/usdGeom/sphere.h>
+#include <pxr/usd/usdGeom/xform.h>
+
+PXR_NAMESPACE_USING_DIRECTIVE
 
 #include <algorithm>
 #include <array>
--- bunny-engine/src/main.cpp
+++ bunny-engine/src/main.cpp
@@ -2,6 +2,19 @@
 #include "pch.h"
 
 int main() {
+	UsdStageRefPtr stage = UsdStage::CreateInMemory();
+	if (stage) {
+		std::cout << "USD smoke test passed - stage created successfully\n";
+
+		// Create a simple prim to verify USD is fully functional
+		UsdGeomXform root = UsdGeomXform::Define(stage, SdfPath("/Root"));
+		if (root) {
+			std::cout << "USD can create prims\n";
+		}
+	} else {
+		std::cerr << "USD smoke test FAILED - could not create stage\n";
+	}
+
 	Application app;
 
 	try {
